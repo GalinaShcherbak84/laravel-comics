858b7118d02eb93b22d0a189f3bba150210caebf
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-08 21:14:46 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,9 @@
 <header>
-    header here
+    <div class="container">
+        <a href="{{ route('home') }}">
+            <img src="{{ asset('images/dc-logo.png')}}" alt="">
+        </a>
+        <a href="">COMICS</a>
+        <a href="">NEWS</a>
+    </div>
 </header>
